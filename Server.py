db63dc3f1e6dc3f68344f40422fa2dc2f23df7a6
root <root@localhost.localdomain>
2019-10-11 00:48:20 -0400

    Add a GUI for client and encryption

--- Server.py
+++ Server.py
@@ -1,19 +1,24 @@
+#!/usr/bin/python3
 import socket
 import sys
 import threading
 import time
+import signal
+from cryptography.fernet import Fernet
 i=0
 class userThread(threading.Thread):
 	def __init__(self,host,portNumber,maxUsersCount):
-                self.host=host
-                self.maxUsersCount=maxUsersCount
-                self.total_user=0
-                self.connecting_users=[]#keep track of connecting users
-                self.portNumber=portNumber
-                self.threads=[]#list of socket thread
-                self.users=[]#list of usernames of the connected clients
-                self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                self.sock.bind((host,portNumber))
+		self.host=host
+		self.maxUsersCount=maxUsersCount
+		self.total_user=0
+		self.connecting_users=[]#keep track of connecting users
+		self.portNumber=portNumber
+		self.threads=[]#list of socket thread
+		self.users=[]#list of usernames of the connected clients
+		self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.sock.bind((host,portNumber))
+		self.shared_key=b'gWEdcvtXUknmH8li0G_Aj0DrtJPyrg9SP38_zG-0o10='
+		self.f=Fernet(self.shared_key)
 	def run(self):
 		self.sock.listen(5)
 		try:
@@ -22,86 +27,100 @@ class userThread(threading.Thread):
 				#if maximum number of connection reached, send back the #busy status
 				#to the client and close its connection
 				if self.total_user==self.maxUsersCount:
-					usrsock.send("#busy")
+					token=self.f.encrypt(b"#busy")
+					usrsock.send(token)
 					usrsock.close()
 				else:
-					usrsock.send("#notbusy")
+					token=self.f.encrypt(b"#notbusy")
+					usrsock.send(token)
 					self.threads.append(usrsock)
 					self.total_user+=1
-					print "Current user count=",self.total_user
+					print("Current user count=",self.total_user)
 					threading.Thread(target=self.thread_handler,args=(usrsock,)).start()
-                        self.sock.close()
+			self.sock.close()
 		except socket.error as err:
-			print err
+			print(err)
 			sys.exit()
-#handle protocol messages and send broadcast messages
-        def thread_handler(self,usrsock):
-	    while True:
-	        try:
-		    resp=usrsock.recv(1024)#receiving data
-		    if resp.startswith('#join'):
-			usr=resp.replace('#join','').lstrip()
-			if usr in self.users:
-                            global i
-                            usr=usr+str(i)
-			    usrsock.send("#usernametaken %s"%usr)
-                            i+=1
-			self.broadcast("#newuser %s"%usr,usrsock)
-                        self.users.append(usr)
-			usrsock.send("#welcome %s"%usr)
-                        #in order to prevent this thread to send 2 packets at the same time
-                        #let it sleep for 1 sec so data can arrive in sequence, not at the same time
-                        time.sleep(1)
-                        print "Current connecting users:"
-                        for usr in self.users:
-                            print usr
-			if len(self.users)>1:
-			    for i in range(0,len(self.users)):
-				usrsock.send("#prevjoined %s"%self.users[i])
-				    #announce to all the clients that a new user has joined
-		    if resp.startswith('#status'):
-			usrsock.send("#statusPosted")
-	    		status=resp.replace('#status','').lstrip()
-	    		#broadcast status to all other users
-	    		self.Ibroadcast("#newStatus %s:%s"%(usr,status))
-	    	    if resp.startswith('#Bye'):
-	    	        self.broadcast("#Leave %s"%usr,usrsock)
-                        #broadcast message to all other users saying that a user is leaving
-	    		usrsock.send("#Bye")
-	    	        self.threads.remove(usrsock)#remove that user thread
-	    	        self.total_user-=1 #subtract 1 from total user
-	    		self.users.remove(usr)#remove username from list
-	    		print "Current user count=",self.total_user
-	    		self.usr=""#set username to empty
-	    		usrsock.close()
-	    		break
-                except IOError as err:
-	    	    print err
-	    	    sys.exit()
+
+	def thread_handler(self,usrsock):
+		while True:
+			try:
+				resp=self.f.decrypt(usrsock.recv(1024)).decode()#receiving data
+				if resp.startswith('#join'):
+					usr=resp.replace('#join','').lstrip()
+					if usr in self.users:
+						global i
+						usr=usr+str(i)
+						token=self.f.encrypt(("#usernametaken %s"%usr).encode())
+						usrsock.send(token)
+						i+=1
+					self.broadcast("#newuser %s"%usr,usrsock)
+					self.users.append(usr)
+					token=self.f.encrypt(("#welcome %s"%usr).encode())
+					usrsock.send(token)
+		        	#in order to prevent this thread to send 2 packets at the same time
+		        	#let it sleep for 1 sec so data can arrive in sequence, not at the same time
+					time.sleep(1)
+					print("Current connecting users:")
+					for usr in self.users:
+						print(usr)
+					if len(self.users)>1:
+						for i in range(0,len(self.users)):
+							if self.users[i]!=usr:
+								token=self.f.encrypt(("#prevjoined %s"%self.users[i]).encode())
+								usrsock.send(token)
+							#announce to all the clients that a new user has joined
+				if resp.startswith('#status'):
+					token=self.f.encrypt("#statusPosted".encode())
+					usrsock.send(token)
+					status=resp.replace('#status','').lstrip()
+					#broadcast status to all other users
+					self.broadcast("#newStatus %s:%s"%(usr,status),usrsock)#changed from Ibroadcast, work but doesn't look good, lets try gui!
+				if resp.startswith('#Bye'):
+					self.broadcast("#Leave %s"%usr,usrsock)
+					#broadcast message to all other users saying that a user is leaving
+					token=self.f.encrypt("#Bye".encode())
+					usrsock.send(token)
+					self.threads.remove(usrsock)#remove that user thread
+					self.total_user-=1 #subtract 1 from total user
+					self.users.remove(usr)#remove username from list
+					print("Current user count=",self.total_user)
+					self.usr=""#set username to empty
+					usrsock.close()
+					break
+			except IOError as err:
+				print(err)
+				sys.exit()
 #broadcast messages to all clients except the connecting one
-        def broadcast(self,message,sock):
-	    for thread in self.threads:
-	        if thread!=sock:
-		    thread.send(message)
-        def Ibroadcast(self,message):
-	    for thread in self.threads:
-		thread.send(message)
+	def broadcast(self,message,sock):
+		for thread in self.threads:
+			if thread!=sock:
+				token=self.f.encrypt(message.encode())
+				thread.send(token)
+
+	def Ibroadcast(self,message):
+		for thread in self.threads:
+			token=self.f.encrypt(message.encode())
+			thread.send(token)
 	
+# def sig_handler(sig, frame):
+# 	print("Interrupt received, closing program and terminating all connections...")
+# 	sys.exit(1)
 def main():
 	maxUsersCount=5
 	portNumber=58888
 	if len(sys.argv)<3:
-		print "Usage: python Server.py <portNumber> <max user count>"
-		print "Now using port number=",portNumber
-		print "Maximum user count=",maxUsersCount
+		print("Usage: python Server.py <portNumber> <max user count>")
+		print("Now using port number=",portNumber)
+		print("Maximum user count=",maxUsersCount)
 	else:
 		portNumber=int(sys.argv[1])
 		maxUsersCount=int(sys.argv[2])
-	print "Server now using port number=%d\nMaximum user count=%d"%(portNumber,maxUsersCount)
+	print("Server now using port number=%d\nMaximum user count=%d"%(portNumber,maxUsersCount))
 	try:
 		threads=userThread('',portNumber,maxUsersCount).run()#start the work	
 	except IOError as err:
-		print err
+		print(err)
 		sys.exit()
 if __name__=='__main__':
 	main()
--- User.py
+++ User.py
@@ -1,116 +1,114 @@
+#!/usr/bin/python3
 import sys
 import socket
 import threading
-class bcolors:
-        PURPLE = '\033[95m'
-        CYAN = '\033[96m'
-        DARKCYAN = '\033[36m'
-        BLUE = '\033[94m'
-        GREEN = '\033[92m'
-        YELLOW = '\033[93m'
-        RED = '\033[91m'
-        BOLD = '\033[1m'
-        UNDERL = '\033[4m'
-        ENDC = '\033[0m'
-        backBlack = '\033[40m'
-        backRed = '\033[41m'
-        backGreen = '\033[42m'
-        backYellow = '\033[43m'
-        backBlue = '\033[44m'
-        backMagenta = '\033[45m'
-        backCyan = '\033[46m'
-        backWhite = '\033[47m'
-
+from cryptography.fernet import Fernet
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+usr=''
 class UserThread(threading.Thread):
-	def __init__(self,host,port):
+	def __init__(self,host,port,ui):
 		self.host=host
 		self.port=port
 		self.username=''
+		self.ui=ui
+		self.shared_key=b'gWEdcvtXUknmH8li0G_Aj0DrtJPyrg9SP38_zG-0o10='
+		self.f=Fernet(self.shared_key)
 		try:
 			self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 			self.sock.connect((host,port))
 		except IOError as ioerr:
-			print ioerr
+			print(ioerr)
 			sys.exit()
 		except socket.error as sockerr:
-			print sockerr
+			print(sockerr)
 			sys.exit()
 	def run(self):
-		status=self.sock.recv(1024)#Receive the server status before making any connection
+		status=self.f.decrypt(self.sock.recv(1024)).decode()#Receive the server status before making any connection
 		if status=="#busy":
-			print "The server is busy, please try again later."
+			print("The server is busy, please try again later.")
 			sys.exit()
 		try:
-    	                #start the two threads
-		        threading.Thread(target=self.receive_server_data,args=(self.sock,)).start()
-		        threading.Thread(target=self.send_usr_input,args=(self.sock,)).start()
+    		#start the thread
+			global usr
+			usr=input("Enter your name:\n")
+			token=self.f.encrypt(("#join %s"%usr).encode())
+			self.sock.send(token)
+			threading.Thread(target=self.receive_server_data,args=(self.sock,)).start()
 		except IOError as ioerr:
-			print ioerr
+			print(ioerr)
 			sys.exit()
-	def send_usr_input(self,sock):
-		usr=raw_input("Enter your name:\n")
-		self.sock.send("#join %s"%usr)
-		try:
-			while True:
-				data=sys.stdin.readline()#read from stdin
-				#status must not be the Exit command or a 1 character
-				if not data.startswith('Exit') and len(data)>1:
-					    self.sock.send("#status %s"%data)
-				elif data.startswith('Exit'):
-					self.sock.send("#Bye")
-					break
-		except IOError as ioerr:
-			print ioerr
-			sys.exit()
-		except socket.error as sockerr:
-			print sockerr
-			sys.exit()
+
+	def sendtext(self):
+		global usr
+		data=self.ui.lineEdit.text()
+		if data!="" and data!="Exit":
+			self.ui.plainTextEdit.appendHtml("<html><b>"+usr+"</b></html>:"+data)
+			token=self.f.encrypt(("#status %s"%data).encode())
+			self.sock.send(token)
+			self.ui.lineEdit.setText("")
+		if data=="Exit":
+			token=self.f.encrypt("#Bye".encode())
+			self.sock.send(token)
+			sys.exit(2)
 #thread for receiving and handling data from server
 	def receive_server_data(self,sock):
 		while True:
-		        data=sock.recv(1024)
+			data=self.f.decrypt(sock.recv(1024)).decode()#what if the message is longer than 1024 bytes?
 			if data.startswith('#newuser'):
-			        self.username=data.replace('#newuser','').lstrip()
-				print "New user "+bcolors.BLUE+self.username+bcolors.ENDC+" has joined!!!"
-    	                #print all current connecting users, minus the one that got just connected
-    		        if data.startswith('#prevjoined'):
-    		    	        l=data.replace('#prevjoined','').lstrip().split()
-    			        for usr in l:
-    				    if usr!=self.username:
-    				        print "New user "+bcolors.BLUE+usr+bcolors.ENDC+" has joined!!!"
-    		        if data.startswith('#usernametaken'):
-    		                print "Username has already taken."
-                                self.username=data.replace('#usernametaken','').lstrip()
-                                print "Your username will be "+bcolors.BLUE+self.username+bcolors.ENDC
-                        if data.startswith('#welcome'):
-    			        usr=data.replace('#welcome','').lstrip()
-    			        print "Welcome user "+bcolors.BLUE+usr+bcolors.ENDC+" to our Social Media App."
-    			        print "To leave enter "+bcolors.RED+"Exit"+bcolors.ENDC+" on a new line."
- 		            #display any broadcasted status 
-    		        if data.startswith('#newStatus'):
-    			        usr_and_stt=data.replace('#newStatus','').lstrip()
-    			        usrinfo=usr_and_stt.split(':')
-    			        usrname=str(usrinfo[0])
-    			        usrstt=str(usrinfo[1])
-    			        print "<"+bcolors.BLUE+usrname+bcolors.ENDC+">:"+ bcolors.PURPLE+usrstt+bcolors.ENDC
-    		        if data.startswith('#Leave'):
-    			        usr=data.replace('#Leave','').lstrip()
-    			        print "\nThe user "+bcolors.BLUE+usr+bcolors.ENDC+" is leaving!!!"
-    		        if data.startswith('#Bye'):
-    			        print "The server has closed the connection."
-    			        sock.close()
-                                break
+				self.username=data.replace('#newuser','').lstrip()
+				self.ui.plainTextEdit.appendPlainText("New user "+self.username+" has joined.")
+    	    	#print all current connecting users, minus the one that got just connected
+			if data.startswith('#prevjoined'):
+				l=data.replace('#prevjoined','').lstrip().split()
+				for usr in l:
+					if usr!=self.username:
+						self.ui.plainTextEdit.appendPlainText("Previously joined user: "+usr)
+			if data.startswith('#usernametaken'):
+				self.ui.plainTextEdit.appendPlainText("Username has already taken.")
+				self.username=data.replace('#usernametaken','').lstrip()
+				self.ui.plainTextEdit.appendPlainText("Your username will be "+self.username)
+			if data.startswith('#welcome'):
+				usr=data.replace('#welcome','').lstrip()
+				self.ui.plainTextEdit.appendPlainText("Welcome user "+usr+" to my Social Media App.")
+				self.ui.plainTextEdit.appendPlainText("To leave enter Exit on a new line.")
+				#display any broadcasted status 
+			if data.startswith('#newStatus'):
+				usr_and_stt=data.replace('#newStatus','').lstrip()
+				usrinfo=usr_and_stt.split(':')
+				usrname=str(usrinfo[0])
+				usrstt=str(usrinfo[1]).rstrip()
+				self.ui.plainTextEdit.appendPlainText(usrname+":"+usrstt)
+			if data.startswith('#Leave'):
+				usr=data.replace('#Leave','').lstrip()
+				self.ui.plainTextEdit.appendPlainText("User "+usr+" has left the chat.")
+			if data.startswith('#Bye'):
+				self.ui.plainTextEdit.appendPlainText("The user has closed the connection.")
+				sock.close()
+				break
+
+class Mychatroom(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.ui=uic.loadUi("chatroom.ui")
+		self.ui.show()
+		portNumber=58888	#Default port
+		host="localhost"	#Default host
+		if len(sys.argv)<3:
+			print("Usage: python User.py <host> <portNumber>")
+			print("Now using host=%s, portNumber=%d"%(host,portNumber))
+		else:
+			host=str(sys.argv[1])
+			portNumber=int(sys.argv[2])
+		self.thread=UserThread(host,portNumber,self.ui)#do the real work
+		self.thread.run()
+		self.ui.pushButton.clicked.connect(self.thread.sendtext)#call the handler function when clicked
+		self.ui.plainTextEdit.setReadOnly(True)#disable text editing to make it a display widget.
+
 def main():
-	portNumber=58888	#Default port
-	host="localhost"	#Default host
-	if len(sys.argv)<3:
-		print bcolors.CYAN
-		print "Usage: python User.py <host> <portNumber>"
-		print "Now using host=%s, portNumber=%d"%(host,portNumber)
-		print bcolors.ENDC
-	else:
-		host=str(sys.argv[1])
-		portNumber=int(sys.argv[2])
-	thread=UserThread(host,portNumber).run()#do the real work
+	app=QApplication(sys.argv)
+	win=Mychatroom()
+	sys.exit(app.exec_())
+
 if __name__=='__main__':
 	main()
